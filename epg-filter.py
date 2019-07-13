00453b3a05685cec459bb8559383bbba949270a0
Redwid <redwid@gmail.com>
2019-07-13 10:45:57 +0100

    Initial submit

---
+++ epg-filter.py
@@ -0,0 +1,438 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+import sys
+import json
+import time
+import requests
+from sh import gunzip
+import xml.etree.ElementTree as ET
+
+from model.model_items import M3uItem, ChannelItem, NameItem, ProgrammeItem
+
+# M3U url
+m3u_url = 'http://192.168.1.64:8000/torrent-telik'
+
+# EPG urls
+tv_epg_urls = ['https://iptvx.one/epg/epg.xml.gz',
+               'http://www.teleguide.info/download/new3/xmltv.xml.gz',
+               'http://programtv.ru/xmltv.xml.gz',
+               'http://epg.it999.ru/edem.xml.gz']
+# tv_epg_urls = ['https://iptvx.one/epg/epg.xml.gz']
+
+# Cache folder
+cache_folder = 'cache'
+
+# Replacement map for channels
+replacement_map = [
+    [['Animal Planet'], 'Animal Planet HD'],
+    [['Da Vinci', 'Да Винчи'], 'Da Vinci'],
+    [['Discovery Россия'], 'Discovery Россия HD'],
+    [['Candy', 'Candy 3D HD'], 'Candy HD'],
+    [['Dorcel'], 'Dorcel HD'],
+    [['HD Life', 'HDL'], 'HD-Life'],
+    [['National Geographic'], 'National Geographic HD'],
+    [['TLC', 'TLC HD'], 'TLC Россия'],
+    [['Travel Channel'], 'Travel channel HD'],
+    [['Travel+Adventure'], 'Travel+Adventure HD'],
+    [['Outdoor channel', 'Outdoor HD'], 'Outdoor channel HD'],
+    [['History2', 'H2 HD'], 'History2 HD'],
+    [['Живая природа'], 'Живая природа HD'],
+    [['Россия-Планета (Европа)', 'РТР-Планета Европа'], 'РТР-Планета'],
+    [['Cartoon Network', 'Cartoon Network HD'], 'Cartoon network'],
+    [['7 канал'], '7 канал (Казахстан)'],
+    [['Беларусь 1 HD'], 'Беларусь 1'],
+    [['Беларусь 2 HD'], 'Беларусь 2'],
+    [['Беларусь 3 HD'], 'Беларусь 3'],
+    [['Беларусь 5 HD'], 'Беларусь 5'],
+    [['Интер', 'Iнтер'], 'Интер'],
+    [['Paramount Channel HD'], 'Paramount Сhannel HD'],
+    [['НТВ-Мир'], 'НТВ Мир'],
+    [['Мир', 'Мир HD'], 'МирТВ'],
+    [['Мир Premium'], 'Мир HD'],
+    [['Астана'], 'Астана ТВ'],
+    [['Первый HD +4'], 'Первый канал HD (+4)'],
+    [['Первый канал +2'], 'Первый канал (+2)'],
+    [['5 канал Россия'], '5 канал (Россия)'],
+    [['Техно 24'], '24Техно'],
+    [['Россия 1 +2'], 'Россия 1 (+2)'],
+    [['BBC', 'BBC World News'], 'BBC World News Europe'],
+    [['О!'], 'Канал О!'],
+    [['НСТ', 'НСТ Страшное'], 'НСТ (Страшное)'],
+    [['Sony Sci-Fi'], 'AXN Sci-Fi'],
+    [['TiJi'], 'TiJi Россия'],
+    [['Шант Premium HD', 'Shant Premium HD'], 'Shant HD'],
+    [['9 канал Израиль'], '9 канал (Израиль)'],
+    [['History Russia'], 'History Россия HD'],
+    [['Nu.ART TV', 'Ню Арт'], 'Nuart'],
+    [['VH1', 'VH1 European'], 'VH1 Europe'],
+    [['Viasat Sport HD'], 'Viasat Sport HD Россия'],
+    [['Матч! HD', 'Матч HD', 'МатчТВ HD'], 'Матч ТВ HD'],
+    [['Наш футбол HD'], 'НТВ+ Наш футбол HD'],
+    [['RT Д Русский'], 'RT Д HD'],
+    [['Hustler HD'], 'Hustler HD Europe'],
+    [['Шансон-TB'], 'Шансон ТВ'],
+    [['НТВ +2'], 'НТВ (+2)'],
+    [['НТВ'], 'НТВ HD'],
+    [['ОНТ', 'ОНТ HD'], 'ОНТ Беларусь'],
+    [['Первый канал', 'Первый HD'], 'Первый канал HD'],
+    [['Рен ТВ', 'РенТВ'], 'РЕН ТВ'],
+    [['Россия 1 HD'], 'Россия HD'],
+    [['ТНТ'], 'ТНТ HD'],
+    [['Disney'], 'Канал Disney'],
+    [['A HOME OF HBO 1', 'Amedia Hit'], 'Amedia Hit HD'],
+    [['A HOME OF HBO 2', 'Amedia Premium HD'], 'Amedia Premium'],
+    [['Fox Life', 'FoxLife'], 'Fox Life Россия'],
+    [['Fox', 'Fox HD'], 'Fox Россия'],
+    [['Paramount Comedy'], 'Paramount Comedy Россия'],
+    [['TV1000', 'TV 1000 East'], 'TV1000 East'],
+    [['TV1000 Comedy', 'VIP Comedy'], 'ViP Comedy HD'],
+    [['TV1000 Premium HD', 'VIP Premiere HD'], 'ViP Premiere HD'],
+    [['Дом кино Премиум'], 'Дом кино Премиум HD'],
+    [['КиноТВ', 'Кино ТВ'], 'КиноТВ HD'],
+    [['Шокирующее'], 'Кинопоказ HD1'],
+    [['Комедийное'], 'Кинопоказ HD2'],
+    [['Наш детектив', 'Наше крутое HD'], 'Наш Детектив HD'],
+    [['Наш кинороман'], 'Наш Кинороман HD'],
+    [['Кинопремьера'], 'Кинопремьера HD'],
+    [['TV XXI', 'TV21'], 'ТВ XXI'],
+    [['Европа Плюс ТВ'], 'Europa Plus TV'],
+    [['MTV Russia'], 'MTV Россия'],
+    [['Матч! Игра', 'Матч Игра'], 'Матч! Игра HD'],
+    [['Матч! Арена', 'Матч Арена'], 'Матч! Арена HD'],
+    [['Матч! Футбол 1', 'Матч Футбол 1'], 'Матч! Футбол 1 HD'],
+    [['Матч! Футбол 2', 'Матч Футбол 2'], 'Матч! Футбол 2 HD'],
+    [['Матч! Футбол 3', 'Матч Футбол 3'], 'Матч! Футбол 3 HD'],
+    [['Сетанта Спорт + HD', 'Setanta Sports'], 'Сетанта Спорт HD'],
+    [['Setanta Sports+', 'Сетанта Спорт+'], 'Сетанта Спорт + HD'],
+    [['Eurosport 2'], 'Eurosport 2 HD'],
+    [['А1', 'Amedia 1 HD'], 'A1'],
+    [['TV3 LT', 'TV3 Литва'], 'TV3'],
+    [['LRT Televizija'], 'LRT'],
+    [['ТВ3', 'ТВ-3'], 'ТВ3 Россия'],
+    [['Мульт и музыка', 'МультиМузыка'], 'Страна']
+]
+
+
+def add_custom_entries(channel_item):
+    list = channel_item.display_name_list
+
+    with open('replacement_map.json', 'w') as json_file:
+        json_file.write(json.dumps(replacement_map))
+
+    for item in replacement_map:
+        if insert_value_if_needed(list, item[0], item[1]):
+            if item[1] == 'МирТВ':
+                delete_from_list(list, 'Мир HD')
+            return
+
+    pass
+
+
+def insert_value_if_needed(list, list_to_check, value_to_insert):
+    for value0 in list:
+        if value0.text in list_to_check:
+            value = get_value_from_list(value_to_insert, list)
+            if(value is None):
+                list.insert(0, NameItem(value_to_insert))
+            else:
+                list.remove(value)
+                list.insert(0, value)
+            return True
+    return False
+
+
+def delete_from_list(list, item):
+    value = get_value_from_list(item, list)
+    if value is not None:
+        list.remove(value)
+
+
+def get_value_from_list(value, list):
+    for item in list:
+        if item.text == value:
+            return item
+
+    return None
+
+
+def download_file(url, file_name):
+    print('download_file(' + url + ', ' + file_name)
+
+    file_name = cache_folder + '/' + file_name
+    file_name_no_gz = file_name.replace('.gz', '')
+
+    etag_file_name, file_extension = os.path.splitext(file_name)
+    etag_file_name = etag_file_name + '.etag'
+    data = load_last_modified_data(etag_file_name)
+    headers = {}
+    if data is not None:
+        file_name_no_gz = file_name.replace('.gz', '')
+        if os.path.exists(file_name_no_gz):
+            if data['last_modified'] != 'None':
+                headers['If-None-Match'] = data['etag']
+            if data['last_modified'] != 'None':
+                headers['If-Modified-Since'] = data['last_modified']
+
+    if not os.path.exists(cache_folder):
+        os.makedirs(cache_folder)
+
+    get_response = requests.get(url, headers=headers, stream=True)
+    if get_response.status_code == 304:
+        print('download_file() ignore as file "Not Modified"')
+        return file_name_no_gz
+
+    store_last_modified_data(etag_file_name, get_response.headers)
+
+    with open(file_name, 'wb') as f:
+        for chunk in get_response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+    print('download_file done: ' + file_name + ', file size: ' + str(os.path.getsize(file_name)))
+    return file_name
+
+
+def store_last_modified_data(file_name, headers):
+    print('store_last_modified_data(' + file_name + ')')
+
+    data = {'etag': str(headers.get('ETag')), 'last_modified' : str(headers.get('Last-Modified'))}
+    print('store_last_modified_data(), data: ' + str(data))
+    with open(file_name, 'w') as json_file:
+        json_file.write(json.dumps(data))
+
+
+def load_last_modified_data(file_name):
+    try:
+        with open(file_name) as json_file:
+            data = json.load(json_file)
+            return data
+    except:
+        print('ERROR can\'t read file: ' + file_name)
+    return None
+
+
+def download_m3u():
+    print('download_m3u()')
+    file_name = download_file(m3u_url, 'm3u.m3u')
+    print('download_m3u() done')
+    return file_name
+
+
+def download_epgs():
+    print('download_epgs()')
+    index = 1
+    downloaded = []
+    for url in tv_epg_urls:
+        try:
+            file_name = 'epg-' + str(index) + '.xml.gz'
+            file_name = download_file(url, file_name)
+
+            if file_name.endswith('.gz'):
+                xml_file_name = file_name.replace('.gz', '')
+                if os.path.exists(xml_file_name):
+                    os.remove(xml_file_name)
+                gunzip(file_name)
+                file_name = xml_file_name
+
+            downloaded.append(file_name)
+            print('download_epg done, xml size: ' + str(os.path.getsize(file_name)))
+        except:
+            print('ERROR in download_epg', sys.exc_info()[0])
+        index = index + 1
+    return downloaded
+
+
+def load_xmlt(m3u_entries, epg_file, channel_list, programme_list):
+    print('load_xmlt(' + epg_file + ')')
+
+    tree = ET.parse(epg_file)
+    root = tree.getroot()
+
+    for item in root.findall('./channel'):
+        channel_item = ChannelItem(item)
+
+        add_custom_entries(channel_item)
+
+        value = is_channel_present_in_list_by_name(channel_list, channel_item)
+        channel_in_m3u = is_channel_present_in_m3u(channel_item, m3u_entries)
+        if value is None and channel_in_m3u:
+            channel_list.append(channel_item)
+
+        # if value is not None and channel_in_m3u:
+        #     merge_values(value, channel_item)
+
+    for item in root.findall('./programme'):
+        if is_channel_present_in_list_by_id(channel_list, item.attrib['channel']):
+            program_item = ProgrammeItem(item)
+            programme_list.append(program_item)
+
+    print('load_xmlt(), channel_list size: ' + str(len(channel_list)))
+    print('load_xmlt(), programme_list size: ' + str(len(programme_list)))
+
+
+def merge_values(channel_0, channel_1):
+    display_name_list_0 = channel_0.display_name_list
+    display_name_list_1 = channel_1.display_name_list
+    for name_1 in display_name_list_1:
+        if name_1 not in display_name_list_0:
+            display_name_list_0.append(name_1)
+
+    if 'icon' not in display_name_list_0 and 'icon' in display_name_list_1:
+        display_name_list_0.icon = display_name_list_1.icon
+
+
+def parse_m3u(m3u_filename):
+    print('parse_m3u(' + m3u_filename + ')')
+    m3u_entries = []
+    m3u_file = open(m3u_filename, 'r')
+    line = m3u_file.readline()
+
+    if '#EXTM3U' not in line:
+        print('ERROR in parse_m3u(), file does not start with #EXTM3U, it does not appear to be an M3U file')
+        return m3u_entries
+
+    entry = M3uItem(None)
+
+    for line in m3u_file:
+        line = line.strip()
+        if line.startswith('#EXTINF:'):
+            m3u_fields = line.split('#EXTINF:-1 ')[1]
+            entry = M3uItem(m3u_fields)
+        elif len(line) != 0:
+            entry.url = line
+            if M3uItem.is_valid(entry, True):
+                m3u_entries.append(entry)
+            entry = M3uItem(None)
+
+    m3u_file.close()
+    print('parse_m3u(), m3u_entries size: ' + str(len(m3u_entries)))
+    return m3u_entries
+
+
+def is_channel_present_in_m3u(channel_item, m3u_entries):
+    list = channel_item.display_name_list
+    for value in m3u_entries:
+        for display_name in list:
+            #if 'Paramount Сhannel HD' == value['tvg-name']:
+            #    print('!')
+            if compare(display_name.text, value.name) or compare(display_name.text, value.tvg_name):
+                return True
+    return False
+
+
+def is_channel_present_in_list_by_id(channel_list, channel_item):
+    for value in channel_list:
+        if value.id == channel_item:
+            return True
+    return False
+
+
+def is_channel_present_in_list_by_name(channel_list, channel_item):
+    list0 = channel_item.display_name_list
+    for channel in channel_list:
+        list1 = channel.display_name_list
+        for name1 in list0:
+            for name2 in list1:
+                if compare(name1.text, name2.text):
+                    return channel
+    return None
+
+
+def compare(string1, string2):
+    # if type(string1) is dict:
+    #     string1 = string1['text']
+
+    # if type(string1) is ChannelItem:
+    #     string1 = string1.text
+
+    # if type(string1) is NameItem:
+    #     string1 = string1.text
+
+    # if type(string2) is dict:
+    #     string2 = string2['text']
+
+    # if type(string2) is ChannelItem:
+    #     string2 = string2.text
+    #
+    # if type(string2) is NameItem:
+    #     string2 = string2.text
+
+    if string1 == string2 or string1.lower() == string2.lower():
+        return True
+
+    return False
+
+
+def writeXml(channel_list, programme_list):
+    print('writeXml()')
+
+    tv = ET.Element("tv")
+
+    for channel in channel_list:
+        channel.to_et_sub_element(tv)
+
+    for programme in programme_list:
+        programme.to_et_sub_element(tv)
+
+    tree = ET.ElementTree(tv)
+    file_name = cache_folder + '/epg-all.xml'
+
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+    tree.write(file_name, encoding='utf-8', xml_declaration=True)
+    print('writeXml() done, file size: ' + str(os.path.getsize(file_name)))
+
+
+if __name__ == '__main__':
+    print('main()')
+    start_time = time.time()
+
+    m3u_file = download_m3u()
+    # m3u_file = cache_folder + '/m3u.m3u'
+    m3u_entries = parse_m3u(m3u_file)
+    downloaded = download_epgs()
+    # downloaded = [cache_folder + '/epg-1.xml', cache_folder + '/epg-2.xml', cache_folder + '/epg-3.xml', cache_folder + '/epg-4.xml']
+
+    channel_list = []
+    programme_list = []
+    for file in downloaded:
+        load_xmlt(m3u_entries, file, channel_list, programme_list)
+
+    print('Not preset:')
+    counter = 0
+    for value in m3u_entries:
+
+        # if 'Paramount Сhannel HD' == value.tvg_name:
+        #     print('!')
+        found = False
+        for channel in channel_list:
+            found = False
+            display_name_list = channel.display_name_list
+            for display_name in display_name_list:
+                if compare(display_name.text, value.name) or compare(display_name.text, value.tvg_name):
+                    found = True
+                    break
+            if found:
+                break
+        if not found:
+            print('  ' + str(value))
+            counter = counter + 1
+    print('Not preset, counter: ' + str(counter))
+
+    # print('Empty:')
+    # for programme in programme_list:
+    #     title_list = programme['titles']
+    #     if title_list:
+    #         pass
+    #     else:
+    #         print(programme)
+    #
+    #     desc_list = programme['descs']
+    #     if desc_list:
+    #         pass
+    #     else:
+    #         print(programme)
+
+    writeXml(channel_list, programme_list)
+    print("main(), done: %s seconds" % (time.time() - start_time))
---
+++ model/model_items.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import re
+import xml.etree.ElementTree as ET
+
+class M3uItem:
+    def __init__(self, m3u_fields):
+        self.tvg_name = None
+        self.tvg_id = None
+        self.tvg_logo = None
+        self.group_title = None
+        self.name = None
+        self.url = None
+        self.group_idx = 0
+        self.channel_idx = -1
+
+        if m3u_fields is not None:
+            try:
+                self.tvg_name = re.search('tvg-name="(.*?)"', m3u_fields, re.IGNORECASE).group(1)
+            except AttributeError as e:
+                pass
+            try:
+                self.tvg_id = re.search('tvg-id="(.*?)"', m3u_fields, re.IGNORECASE).group(1)
+            except:
+                pass
+            try:
+                self.tvg_logo = re.search('tvg-logo="(.*?)"', m3u_fields, re.IGNORECASE).group(1)
+            except AttributeError as e:
+                pass
+            try:
+                self.group_title = re.search('group-title="(.*?)"', m3u_fields, re.IGNORECASE).group(1)
+            except AttributeError as e:
+                pass
+            try:
+                self.name = m3u_fields.split("\",")[1]
+            except AttributeError as e:
+                pass
+
+    def is_valid(self, allow_no_tvg_id):
+        isvalid = self.tvg_name is not None and self.tvg_name != "" and \
+                  self.group_title is not None and self.group_title != ""
+        if not allow_no_tvg_id:
+            isvalid = isvalid and self.tvg_id is not None and self.tvg_id != ""
+        return isvalid
+
+    def __str__(self):
+        return 'M3uItem[name:' + str(self.name) + ', group_title:' + str(self.group_title) + \
+               ', tvg_name:' + str(self.tvg_name) + ', tvg_id:' + str(self.tvg_id) + \
+               ', tvg_logo:' + str(self.tvg_logo) + ']'
+
+
+class ChannelItem:
+    def __init__(self, xmlt_fields):
+        self.id = None
+        self.text = None
+        self.icon = None
+        self.display_name_list = []
+
+        self.id = xmlt_fields.attrib['id']
+
+        for child in xmlt_fields:
+            if child.tag == 'display-name':
+                display_name = NameItem(None, None, child)
+                if display_name.text is not None:
+                    self.display_name_list.append(display_name)
+
+            else:
+                if child.tag == 'icon':
+                    self.icon = child.attrib['src']
+
+    def to_et_sub_element(self, root):
+        item = ET.SubElement(root, 'channel', id=self.id)
+        display_name_list = self.display_name_list
+        for display_name in display_name_list:
+            add_sub_element('display-name', display_name, item)
+        if self.icon is not None:
+            ET.SubElement(item, 'icon').text = self.icon
+
+    def __str__(self):
+        return 'ChannelItem[id:' + str(self.id) + ', text:' + str(self.text) + \
+               ', display_name_list:' + ', '.join(map(str, self.display_name_list)) + ', icon:' + str(self.icon) + ']'
+
+
+class NameItem:
+    def __init__(self, text, lang=None, xmlt_fields=None):
+        self.lang = lang
+        self.text = text
+        if xmlt_fields is not None and xmlt_fields.text is not None:
+            self.text = xmlt_fields.text
+            if 'lang' in xmlt_fields.attrib:
+                self.lang = xmlt_fields.attrib['lang']
+
+    def __str__(self):
+        return 'NameItem[lang:' + str(self.lang) + ', text:' + str(self.text) + ']'
+
+
+class ProgrammeItem:
+    def __init__(self, xmlt_fields):
+        self.start = xmlt_fields.attrib['start']
+        self.stop = xmlt_fields.attrib['stop']
+        self.channel = xmlt_fields.attrib['channel']
+
+        self.title_list = []
+        self.desc_list = []
+        self.category_list = []
+        for child in xmlt_fields:
+            if child.tag == 'title':
+                title = NameItem(None, None, child)
+                self.title_list.append(title)
+            else:
+                if child.tag == 'desc':
+                    desc = NameItem(None, None, child)
+                    self.desc_list.append(desc)
+                else:
+                    if child.tag == 'category':
+                        category = NameItem(None, None, child)
+                        self.category_list.append(category)
+
+    def to_et_sub_element(self, root):
+        item = ET.SubElement(root, 'programme', start=self.start, stop=self.stop,
+                             channel=self.channel)
+        for title in self.title_list:
+            add_sub_element('title', title, item)
+        for descs in self.desc_list:
+            add_sub_element('desc', descs, item)
+        for category in self.category_list:
+            add_sub_element('category', category, item)
+
+
+def add_sub_element(name, item, root):
+    if item.lang is not None:
+        ET.SubElement(root, name, lang=item.lang).text = item.text
+    else:
+        ET.SubElement(root, name).text = item.text
